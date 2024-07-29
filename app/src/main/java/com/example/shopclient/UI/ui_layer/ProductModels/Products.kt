0d0ce8c8ed5a05d9b4ddc5d3c55fbfc6eb7a818f
Priyanka-Das-0 <dasp43689@gmail.com>
2024-07-29 23:47:17 +0530

    Final

---
+++ app/src/main/java/com/example/shopclient/UI/ui_layer/ProductModels/Products.kt
@@ -0,0 +1,17 @@
+package com.example.shopclient.UI.ui_layer.ProductModels
+
+
+data class Products(
+    var productDisplayImage: String?=null,
+    var productDisplayImages: ArrayList<String>?= arrayListOf<String>(),
+    var productName: String?=null,
+    var productDes: String?=null,
+    var productPrice: Long?=null,
+    var productDiscountPercent: Long?=null,
+    var productSize: String?=null,
+    var productColor: List<ProductColor>?=null,
+    var category:String?=null,
+    var addedTimeStamp:Long?=null
+)
+
+data class ProductColor(var colorName: String?=null, var colorCode: Int?=null)
---
+++ app/src/test/java/com/example/shopclient/test.kt
@@ -0,0 +1,5 @@
+package com.example.shopclient
+
+fun main(){
+
+}
